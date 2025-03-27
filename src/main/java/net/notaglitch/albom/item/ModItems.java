0ef34b24c6a102334dfa6e2dfd80dcaf1a42849e
Andromeda <andromeda@notaglitch.net>
2025-03-27 18:45:01 -0400

    Added Round Disc Resource Pack for Visual Overhaul

--- src/main/java/net/notaglitch/albom/item/ModItems.java
+++ src/main/java/net/notaglitch/albom/item/ModItems.java
@@ -5,15 +5,16 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import net.notaglitch.albom.ALittleBitOfMusic;
 import net.minecraft.registry.Registry;
 import net.notaglitch.albom.sound.ModSounds;
 
 public class ModItems {
     public static final Item MUSIC_DISC_SPELUNK = registerItem("music_disc_spelunk",
-            new Item(new Item.Settings().jukeboxPlayable(ModSounds.SPELUNK_KEY).maxCount(1)));
+            new Item(new Item.Settings().rarity(Rarity.RARE).jukeboxPlayable(ModSounds.SPELUNK_KEY).maxCount(1)));
     public static final Item MUSIC_DISC_RADIANCE = registerItem("music_disc_radiance",
-            new Item(new Item.Settings().jukeboxPlayable(ModSounds.RADIANCE_KEY).maxCount(1)));
+            new Item(new Item.Settings().rarity(Rarity.RARE).jukeboxPlayable(ModSounds.RADIANCE_KEY).maxCount(1)));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(ALittleBitOfMusic.MOD_ID, name), item);
